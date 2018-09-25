dd0c579b24f216baf9c6d2cfb65ffa21a3eafff1
Juan Disteffano <disteffanojuan@gmail.com>
2018-09-25 12:10:13 -0300

    Se realizaron los ejercicios propuestos en la seccion Js Data Structures: Objects and Arrays

--- src/week1/days2-5/excercises.js
+++ src/week1/days2-5/excercises.js
@@ -133,11 +133,17 @@ Define a recursive function isEven corresponding to this description. The functi
 Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
 */
 
-
-
-
-
-
+function isEven(param){
+    if(param == 0){
+        return true;
+    }else if(param == 1){
+        return false;
+    }else if (param < 0){
+        return isEven(-param);
+    }else{
+        return isEven(param - 2)
+    }
+}
 
 
 /*
@@ -148,3 +154,167 @@ Write a function countBs that takes a string as its only argument and returns a 
 
 Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
 */
+
+function countBs (cadena){
+    var count = 0;
+    for(var i=0; i<cadena.length; i++){
+        if(cadena[i] == "B"){
+            count++;
+        }
+    }
+    return count;
+}
+
+function countChar(cadena, letra){
+    var count = 0;
+    for(var i=0; i<cadena.length; i++){
+        if(cadena[i] == letra){
+            count++;
+        }
+    }
+    return count;
+}
+
+
+
+/*
+The sum of a range
+The introduction of this book alluded to the following as a nice way to compute the sum of a range of numbers:
+
+console.log(sum(range(1, 10)));
+Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
+
+Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program and see whether it does indeed return 55.
+
+As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+*/
+
+function range(start, end){
+    var vecRang = [];
+    for(var i=start; i<=end; i++){
+        vecRang.push(i);
+    }
+    return vecRang;
+}
+
+function sum(vec){
+    var resSum = 0;
+    for(var i = 0; i<vec.length; i++){
+        resSum += vec[i];
+    }
+    return resSum;
+}
+
+function rangeMod(start, end, step){
+    var vecRang = [];
+    if(step == undefined){
+        step = 1;
+    } 
+    if(step > 0){
+        for(var i=start; i<=end; i += step){
+            vecRang.push(i);
+        }
+    }else{
+        for(var i=start; i>=end; i += step){
+            vecRang.push(i);
+        }
+    }
+    return vecRang;
+}
+
+/*
+Reversing an array
+Arrays have a reverse method that changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements. Neither may use the standard reverse method.
+
+Thinking back to the notes about side effects and pure functions in the previous chapter, which variant do you expect to be useful in more situations? Which one runs faster?
+*/
+
+function reverseArray (array){
+    var arrayRes = [];
+    for(var i = array.length- 1; i >= 0; i--){
+        arrayRes.push(array[i]);
+    }
+    return arrayRes;
+}
+
+function reverseArrayInPlace (array){
+    for(var i = 0; i < array.length/2; i++){
+        var swap = array[i];
+        array[i] = array[array.length -1 - i];
+        array[array.length -1 - i] = swap;
+    }
+    return array;
+}
+
+
+/*
+A list
+Objects, as generic blobs of values, can be used to build all sorts of data structures. A common data structure is the list (not to be confused with array). A list is a nested set of objects, with the first object holding a reference to the second, the second to the third, and so on.
+
+let list = {
+  value: 1,
+  rest: {
+    value: 2,
+    rest: {
+      value: 3,
+      rest: null
+    }
+  }
+};
+The resulting objects form a chain, like this:
+
+A linked list
+A nice thing about lists is that they can share parts of their structure. For example, if I create two new values {value: 0, rest: list} and {value: -1, rest: list} (with list referring to the binding defined earlier), they are both independent lists, but they share the structure that makes up their last three elements. The original list is also still a valid three-element list.
+
+Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. Also write a listToArray function that produces an array from a list. Then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element.
+
+If you haven’t already, also write a recursive version of nth.
+*/
+
+function arrayToList (array){
+    var listRes = null;
+    for(var i=array.length-1; i>=0; i--){
+        listRes = {value: array[i],
+                    rest: listRes};
+    }
+    return listRes;
+}
+
+function listToArray(list){
+    var arrayRes = [];
+    while(list != null){
+        arrayRes.push(list.value);
+        list = list.rest;
+    }
+    return arrayRes;
+}
+
+function prepend (val, list){
+    return {value: val, rest: list};
+}
+
+function nth (list, number){
+    var res;
+    var count = 0;
+    while(list != null){
+        if(count == number){
+            res = list.value;
+        }
+        count++;
+        list = list.rest;
+    }
+    return res;
+}
+
+
+/*
+Deep comparison
+The == operator compares objects by identity. But sometimes you’d prefer to compare the values of their actual properties.
+
+Write a function deepEqual that takes two values and returns true only if they are the same value or are objects with the same properties, where the values of the properties are equal when compared with a recursive call to deepEqual.
+
+To find out whether values should be compared directly (use the === operator for that) or have their properties compared, you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. But you have to take one silly exception into account: because of a historical accident, typeof null also produces "object".
+
+The Object.keys function will be useful when you need to go over the properties of objects to compare them.
+*/
+
